3e68c9bc20a33fd1952e6bcc7621cc9f316c18f8
Antonio Barros <antoniobarrosandb@gmail.com>
2026-05-28 23:17:06 -0300

    docs: adiciona explicação sobre operadores de comparação

---
+++ 01- basico/05_operadores_comparacao.py
@@ -0,0 +1,32 @@
+# operadores de comparação basicamente são simbolos usados para fazer perguntas para o pyhton sobre a relação entre dois valores, o resultado é sempre booleano, ou seja verdadeiro ou falso.
+
+# 1 == (igual) compara se os valores são iguais.
+x = 5
+y = 5
+print(x == y) # retorna True porque os valores são iguais.
+
+# 2 != (diferente) compara se os valores são diferentes.
+a = 10
+b = 20
+print(a != b) # a é diferente de b ?, retorna True.
+
+# 3 > (maior que) compara se o valor da esquerda é maior que o valor da direita.
+c = 15
+d = 10
+print(c > d) # c é maior que d ?, retorna True.
+
+# 4 < (menor que) compara se o valor da esquerda é menor que o valor da direita.
+e = 8
+f = 12
+print(e < f) # True porque e é menor que f.
+
+# 5 >= (maior ou ugual) compara se o valor da esquerda é maior ou igual ao valor da direita.
+g = 7
+h = 7
+print(g >= h) # g é maior ou igual a h ?, retorna True.
+
+# 6 <= (menor ou igual) parecido com o maior ou igual mas compara se o valor da esquerda é menor ou igual ao valor da direita.
+i = 3
+j = 5
+print(i <= j) # i é menor ou igual a j ?, True.
+
