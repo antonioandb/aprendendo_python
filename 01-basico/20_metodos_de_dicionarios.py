1d2dbaa5176654e0f8d541224f2bbe38310277e5
Antonio Barros <antoniobarrosandb@gmail.com>
2026-06-09 15:20:12 -0300

    docs: adiciona mais exemplos de metodos de dicionartios

--- 01-basico/20_metodos_de_dicionarios.py
+++ 01-basico/20_metodos_de_dicionarios.py
@@ -34,3 +34,34 @@ print(pessoa.setdefault("nome", "nome_padrão"))
 pessoa.setdefault("interesses", []).append("automação")
 # mas se imprimir a lista se verifica que ela foi alterada
 print(pessoa["interesses"])
+
+# update atualiza uma chave e criar se não existir
+pessoa.update(sobrenome = "Andrade", profissao = "programador")
+print(pessoa)
+
+# pop: deleta uma chave e retorna o valor dela
+nome_removido = pessoa.pop("nome")
+print(f"chave: {nome_removido} deletada")
+
+# delete: remove e retorna o último par chave-valor inserido
+deletado = pessoa.popitem()
+print(f"item deletado {deletado}")
+
+print("--" * 10)
+print(pessoa)
+
+
+# resumo:
+#| Método         | O que faz                                 |
+#| -------------- | ----------------------------------------- |
+#| `get()`        | Consulta segura                           |
+#| `setdefault()` | Consulta e cria se não existir            |
+#| `keys()`       | Retorna as chaves                         |
+#| `values()`     | Retorna os valores                        |
+#| `items()`      | Retorna chave e valor                     |
+#| `update()`     | Atualiza ou adiciona chaves               |
+#| `pop()`        | Remove uma chave e retorna seu valor      |
+#| `popitem()`    | Remove e retorna o último par chave-valor |
+#| `del`          | Remove uma chave sem retornar nada        |
+
+
