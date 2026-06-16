58d473d92d23f5ecb9588110f09b5d8c7654fde0
Antonio Barros <antonioeduardo.andr@gmail.com>
2026-06-16 06:57:39 -0300

    faz algumas correções ortograficas

--- 01-basico/21_set.py
+++ 01-basico/21_set.py
@@ -1,4 +1,4 @@
-# set é um conjunto que não permite repetições, ele não pussuem inices a posição na memoria é arbitrária.
+# set é um conjunto que não permite repetições, ele não pussuem indices a posição na memoria é arbitrária.
 # por causa diso ele não suporta fatiamento e acesso por posição [].
 
 frutas = {"banana", "maçã", "laranja", "abacate"}
@@ -21,7 +21,7 @@ print(frutas)
 frutas.discard("uva")
 print(frutas)
 
-# remove: seelhante ao discard as gera erro caso o item não existir.
+# remove: semelhante ao discard mas gera erro caso o item não existir.
 #frutas.remove("uva") gerou um erro
 frutas.remove("limão")
 print(frutas)
@@ -30,7 +30,7 @@ print(frutas)
 pop = frutas.pop()
 print(f"pop: {pop}")
 
-# clear: faz a limpeza do set removeno tudo
+# clear: faz a limpeza do set removendo tudo
 frutas.clear()
 print(frutas)
 
@@ -47,28 +47,28 @@ set2 = {2, 3, 4}
 união = set1.union(set2)
 print(f"união: {união}")
 
-# tambem é possivel usar o operador | (pipe)
+# também é possível usar o operador | (pipe)
 união = set1 | set2 
 
 # INTERSECÇÂO (intersection): retorna um novo set apenas com os elementos que estão em ambos os sets
 intersecção = set1.intersection(set2) 
 print(f"intersecção: {intersecção}")
 
-# tabem é possivel usar o operador & (ampersand)
+# também é possível usar o operador & (ampersand)
 intersecção = set1 & set2
 
 # DIFÊNCIA (difference): retorna um novo set com os elementos que estão no primeiro set e não no segundo set
 diferença = set1.difference(set2)
 print(f"diferença: {diferença}")
 
-# tabem é possivel usar o operador - (minus)
+# também é possível usar o operador - (minus)
 diferença = set1 - set2
 
 # SYMMETRIC DIFFERENCE (symmetric_difference): retorna um novo set com os elementos que estão em um ou outro, mas não em ambos
 diferente = set1.symmetric_difference(set2)
 print(f"diferente: {diferente}")
 
-# tabem é possivel usar o operador ^ (caret)
+# também é possível usar o operador ^ (caret)
 diferente = set1 ^ set2 
 
 
